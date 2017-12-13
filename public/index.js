78d52904c9ae0af1e5d274d4d7d1bcefd5267604
Miguel <miguelmoutela@hotmail.com>
2017-12-13 17:03:23 +0000

    added requests

--- public/index.js
+++ public/index.js
@@ -1,5 +1,29 @@
+let beers = [];
+
 const app = function () {
+
+const getBeerButton = document.querySelector('#get-beer')
+select.addEventListener('click', handleButtonClick);
+
+const url = 'https://api.punkapi.com/v2/beers'
+makeRequest(url, requestComplete)
+}
+
+const handleButtonClick = function() {
 
 }
 
+const makeRequest = function(url, callback) {
+  const request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.send();
+  request.addEventListener('load', callback);
+}
+
+const requestComplete = function(){
+  if (this.status != 200) return;
+  const JsonString = this.responseText;
+  const data = JSON.parse(jsonString);
+  beers = data;
+}
 document.addEventListener('DOMContentLoaded', app);
